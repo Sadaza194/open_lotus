728ddbe8e71049988de2aa4c4e4fed33352ed5d4
larsschoetz <148350882+larsschoetz@users.noreply.github.com>
2024-03-18 22:07:22 -0700

    likertanswer.py model

---
+++ likertanswer.py
@@ -0,0 +1,10 @@
+from django.db import models
+from .answer import Answer
+
+class LikertAnswer(models.Model):
+    likert_id = models.AutoField(primary_key=True)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    value = models.IntegerField()
+
+    def __str__(self):
+        return f"LikertAnswer {self.likert_id} for Answer {self.answer.answer_id}"
