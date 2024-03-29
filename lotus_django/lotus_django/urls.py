ed461dd92ea7d26340bb654bb21af300f95645d2
Hayden Lister <haydenglister@gmail.com>
2024-03-29 16:55:50 -0700

    Created initial test of report display

--- lotus_django/lotus_django/urls.py
+++ lotus_django/lotus_django/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("lotus.urls")),
+    path('report/', include('report.urls'))
 ]
--- lotus_django/report/views.py
+++ lotus_django/report/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def show_report(request):
+    return render(request, 'report.html', {'name': 'Your Name Here'})
