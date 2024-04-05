5e20b57ac60afd76287fe8519bf869f6aa19a6c0
Santiago Daza <s.daza194@gmail.com>
2024-04-05 16:16:01 -0700

    memories changed date to generated; succes message; html base squeleton

--- lotus_django/memories/views.py
+++ lotus_django/memories/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from .models import Memory
+from django.contrib import messages
+from datetime import date
 # Create your views here.
 
 def create_memories(request):
     if request.method == 'POST':
         memory_text = request.POST.get('memory_text')
         emotion = request.POST.get('emotion')
-        date = request.POST.get('date')
-        new_memory = Memory(emotion=emotion, text=memory_text, date=date)
+        memory_date = date.today()
+        new_memory = Memory(emotion=emotion, text=memory_text, date=memory_date)
         new_memory.save()
+        # memory saved
+        messages.success(request, 'Memory saved!')
+        return render(request, 'createMemories.html') # goes  back to the template, replace with a messag
     else:
         return render(request, 'createMemories.html')
