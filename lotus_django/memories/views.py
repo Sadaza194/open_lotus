28533ce025d7ff24555b4c7ee710ef6b3d096cb1
Santiago Daza <s.daza194@gmail.com>
2024-04-05 14:28:29 -0700

    memery data base gather data

--- lotus_django/memories/views.py
+++ lotus_django/memories/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from .models import Memory
 # Create your views here.
+
 def create_memories(request):
-    return render(request, 'createMemories.html')
+    if request.method == 'POST':
+        memory_text = request.POST.get('memory_text')
+        emotion = request.POST.get('emotion')
+        date = request.POST.get('date')
+        new_memory = Memory(emotion=emotion, text=memory_text, date=date)
+        new_memory.save()
+    else:
+        return render(request, 'createMemories.html')
