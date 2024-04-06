cde227773d6f65bc87ed4487790d5aec4607ddca
Lawrence908 <60679325+Lawrence908@users.noreply.github.com>
2024-04-06 16:43:06 -0700

    Added a display to the base.html to show the active user! need to fix the size of it. Applied the visuals to createMemories.html and added a few emotions to the drop down.

--- lotus_django/journal/urls.py
+++ lotus_django/journal/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 from . import views
-from lotus_django.views import home
+# from lotus_django.views import home
 
 urlpatterns = [
     path("", views.journal, name="journal"),
-    path("home/", home, name="home"),
+    # path("home/", home, name="home"),
 
 
 ]
