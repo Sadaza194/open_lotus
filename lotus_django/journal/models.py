caf3c8a29bc5993ddb0b2c08b1fe0fdd9c769e9b
Lawrence908 <60679325+Lawrence908@users.noreply.github.com>
2024-04-06 23:45:32 -0700

    Fixed journal form save. saves to db correctly now. Added a __str__ for Memory

--- lotus_django/journal/models.py
+++ lotus_django/journal/models.py
@@ -1,9 +1,7 @@
 from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Journal(models.Model):
-    # id = models.AutoField(primary_key=True default=1)
     text = models.TextField()
     date = models.DateField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- lotus_django/journal/views.py
+++ lotus_django/journal/views.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.shortcuts import render
 from django.shortcuts import redirect
-from journal.models import Journal
+from django.utils import timezone
 from .forms import JournalForm
 from django.contrib.auth.decorators import login_required
 
@@ -9,36 +9,16 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
-@login_required(login_url='login/')
+@login_required
 def journal(request):
-    form = JournalForm(request.POST or None)
-    if form.is_valid():
-        form.save()
+    if request.method == 'POST':
+        form = JournalForm(request.POST)
+        if form.is_valid():
+            journal = form.save(commit=False)
+            journal.user = request.user
+            journal.date = timezone.now()
+            journal.save()
+            return redirect('home')
+    else:
         form = JournalForm()
-    context = {
-        'form': form
-    }
-    return render(request, "journal.html", context)
-
-
-# from django.contrib.auth import get_user_model
-# User = get_user_model()
-
-# @login_required(login_url='login/')
-# def journal(request):
-#     if request.method == 'POST':
-#         form = JournalForm(request.POST or None)
-#         if form.is_valid():
-#             journal = form.save(commit=False)
-#             if request.user.is_authenticated:  # Check if the user is authenticated
-#                 user = User.objects.get(username=request.user.username)
-#                 entry = Entry.objects.create(user=user, is_journal=True)
-#                 journal.entry = entry
-#                 journal.save()
-#                 return redirect('home')
-#     else:
-#         form = JournalForm()
-#     context = {
-#         'form': form
-#     }
-#     return render(request, "journal.html", context)
+    return render(request, 'journal.html', {'form': form})
