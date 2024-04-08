37848d5cc4f3093c0471b6ba010522f6e6ff308a
Hayden Lister <haydenglister@gmail.com>
2024-04-08 12:22:19 -0700

    Created answer_likert_question and answer_text_question views

--- lotus_django/questions/views.py
+++ lotus_django/questions/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import CreateQuestionForm, AnswerLikertQuestionForm, AnswerTextQuestionForm
 from django.contrib.auth.decorators import login_required, user_passes_test
-from questions.models import LikertAnswer, TextAnswer
+from questions.models import LikertAnswer, TextAnswer, Question
+from django.utils import timezone
 
 # Create your views here.
 
@@ -27,18 +28,44 @@ def questionsBase(response):
 
 
 @login_required(login_url='login/')
-def answer_question(request, *args, **kwargs):
+def answer_likert_question(request, *args, **kwargs):
     if request.method == 'POST':
-        form = CreateQuestionForm(request.POST or None)
+        form = AnswerLikertQuestionForm(request.POST or None)
         if form.is_valid():
-            question = form.save(commit=False)
-            question.creator = request.user
-            question.save()
-            messages.success(request, 'Question saved!')
-            return redirect('create_question')
+            answer = form.save(commit=False)
+            answer.user = request.user
+            answer.date = timezone.now()
+            answer.save()
+            messages.success(request, 'Likert question saved!')
+            return redirect('questionsBase')
     else:
-        form = CreateQuestionForm()
-    return render(request, 'createQuestion.html', {'form': form})
+        likert_questions = Question.objects.all().filter(type='likert')
+        form = AnswerLikertQuestionForm()
+        context = {
+            'form': form,
+            'likert_questions': likert_questions
+        }
+    return render(request, 'answerLikertQuestion.html', context)
+
+@login_required(login_url='login/')
+def answer_text_question(request, *args, **kwargs):
+    if request.method == 'POST':
+        form = AnswerTextQuestionForm(request.POST or None)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.user = request.user
+            answer.date = timezone.now()
+            answer.save()
+            messages.success(request, 'Text answer saved!')
+            return redirect('questionsBase')
+    else:
+        text_questions = Question.objects.all().filter(type='text')
+        form = AnswerTextQuestionForm()
+        context = {
+            'form': form,
+            'text_questions': text_questions
+        }
+    return render(request, 'answerTextQuestion.html', context)
 
 @login_required(login_url='login/')
 def view_answers(request, *args, **kwargs):
